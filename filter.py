d60aaf47ea805c8ab9bf2c7ab46ed6c666a2844a
stablesports711-hue <stablesports711@gmail.com>
2026-07-28 17:11:30 +0600

    Update filter.py

--- filter.py
+++ filter.py
@@ -10,7 +10,7 @@ TARGET_NAME = "Global Super"
 OUTPUT_FILE = "gsl_playlist.m3u8"
 
 # চ্যানেল না পাওয়া গেলে ডিফল্ট লিংক
-DEFAULT_OFFLINE_LINK = "http://example.com/offline.m3u8"
+DEFAULT_OFFLINE_LINK = "https://res.cloudinary.com/qleik3si/video/upload/v1785235285/VN20260728_161756_ev6pow.mp4"
 
 def fetch_content(link_url):
     """লিংক থেকে কনটেন্ট রিড করার ফাংশন"""
